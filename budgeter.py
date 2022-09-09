a298d906c2bbcfc5110d7a864c66c64def1b1ae9
Werefox <jack@wrightus.com>
2022-09-09 10:40:13 -0400

    Fixed the Income instances sharing paychecks

--- budgeter.py
+++ budgeter.py
@@ -25,23 +25,22 @@ class Paycheck():
 
 
 class Income():
-	paychecks = []
-	def __init__(self, name, period=-1) -> None:
+	def __init__(self, name) -> None:
 		self.name = name
-		self.period = period
+		self.paychecks = []
 
-	def add_paycheck(self, paycheck):
-		if type(paycheck) is Paycheck:
-			self.paychecks.append(paycheck)
+	def add_paycheck(self, new_check):
+		if type(new_check) is Paycheck:
+			self.paychecks.append(new_check)
 
-		elif type(paycheck) is float or type(paycheck) is int:
-			self.paychecks.append(Paycheck(paycheck, datetime.now()))
+		elif type(new_check) is float or type(new_check) is int:
+			self.paychecks.append(Paycheck(new_check, datetime.now()))
 
 		else:
-			raise ArgumentTypeError(f"Added paycheck {type(paycheck)} was not type Paycheck, float, or int")
+			raise ArgumentTypeError(f"Added paycheck {type(new_check)} was not type Paycheck, float, or int")
 	
-	def add_paychecks(self, paychecks):
-		for p in paychecks:
+	def add_paychecks(self, new_checks):
+		for p in new_checks:
 			self.add_paycheck(p)
 	
 	def print_paychecks(self):
@@ -49,13 +48,16 @@ class Income():
 			print(p)
 	
 	def get_average(self):
+		if len(self.paychecks) == 0:
+			return 0
+
 		sum = 0
 		for i in range(len(self.paychecks)):
 			sum += self.paychecks[i]
 		return sum / len(self.paychecks)
 	
 	def __str__(self) -> str:
-		return f'{self.name}'
+		return f'{self.name}\n  Average: ${round(self.get_average(), 2)}\n'
 
 check1 = Paycheck(906.29)
 check2 = Paycheck(803.23)
@@ -65,4 +67,13 @@ income = Income("Jack's Income")
 
 income.add_paychecks([check1, check2, check3, 123.45, 900])
 
-print(income.get_average())
+print(income)
+
+income2 = Income("ALDI")
+income2.print_paychecks()
+print(income2)
+income2.add_paycheck(100)
+income2.print_paychecks()
+print(income2)
+print(income)
+print(income.paychecks[-1])
