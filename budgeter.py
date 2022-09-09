ddf2a74a391daebce3da4b9040d8db138b6b50f1
Werefox <jack@wrightus.com>
2022-09-09 09:58:53 -0400

    Added some printing functions and an add_paycheck function to the Income class

--- budgeter.py
+++ budgeter.py
@@ -1,20 +1,45 @@
+from argparse import ArgumentTypeError
 from datetime import datetime
 
 # to budget, we need to know two things: income and expenses
 class Paycheck():
-	def __init__(self, amount, date) -> None:
+	def __init__(self, amount, date=datetime.now()) -> None:
 		self.amount = amount
 		self.date = date
 
 	def __str__(self) -> str:
 		return f'${self.amount}'
-	
 
 
 class Income():
 	paychecks = []
-	def __init__(self, name) -> None:
+	def __init__(self, name, period=-1) -> None:
 		self.name = name
+		self.period = period
 
-check = Paycheck(906.29, datetime.now())
-print(check)
+	def add_paycheck(self, paycheck):
+		if type(paycheck) is Paycheck:
+			self.paychecks.append(paycheck)
+		elif type(paycheck) is float or int:
+			self.paychecks.append(Paycheck(paycheck, datetime.now()))
+		else:
+			raise ArgumentTypeError("Added paycheck was not of type Paycheck, int, or float")
+	
+	def print_paychecks(self):
+		for p in self.paychecks:
+			print(p)
+	
+	def __str__(self) -> str:
+		return f'{self.name}'
+
+check1 = Paycheck(906.29)
+check2 = Paycheck(803.23)
+check3 = Paycheck(855.76)
+
+income = Income("Jack's Income")
+
+income.add_paycheck(check1)
+income.add_paycheck(check2)
+income.add_paycheck(check3)
+
+income.print_paychecks()
